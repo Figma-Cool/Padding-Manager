9af00df3eaf6138a6645c4c118709f172c489ef1
yancymin <senseslofter@gmail.com>
2020-11-29 21:27:00 +0800

    -

--- code.js
+++ code.js
@@ -1,64 +1,57 @@
 figma.showUI(__html__, { width: 300, height: 400 });
 
 const allLayers = figma.currentPage.findAll();
-let $1
+let $1;
 
-const createFrame = item => {
-    let frame = figma.createFrame();
-    let x = item.x;
-    let y = item.y;
-    frame.name = item.name;
-    frame.fills = [];
-    frame.resize(item.width, item.height)
-    item.parent.appendChild(frame);
-    frame.x = x;
-    frame.y = y;
-    frame.appendChild(item);
-    item.x = 0;
-    item.y = 0;
-}
-
-
-
-
-
-
-// const paddings = {
-
-// };
+const createFrame = (item) => {
+  let frame = figma.createFrame();
+  let x = item.x;
+  let y = item.y;
+  frame.name = item.name;
+  frame.fills = [];
+  frame.resize(item.width, item.height);
+  item.parent.appendChild(frame);
+  frame.x = x;
+  frame.y = y;
+  frame.appendChild(item);
+  item.x = 0;
+  item.y = 0;
+};
 
 const paddingControl = (paddings) => {
-    allLayers.forEach(item => {
-        const setPadding = (paddings) => {
-            item.parent.paddingTop = paddings.top
-            item.parent.paddingBottom = paddings.bottom
-            item.parent.paddingLeft = paddings.left
-            item.parent.paddingRight = paddings.right
-        }
-        figma.ui.onmessage = msg => {
-            if (msg.type === '$1') {
-                console.log(msg.value)
-                $1 = msg.value
-                setPadding($1)
-            }
-        }
-        if (item.name.includes(`${paddings}`)) {
-            if (!item.layoutMode) {
-                createFrame(item)
-                item.name = item.name.replace(`${paddings}`, '')
-                item.parent.layoutMode = "VERTICAL"
-                setPadding(paddings)
-            } else {
-                setPadding($1)
-                figma.ui.postMessage({
-                    top: item.paddingTop,
-                    left: item.paddingLeft,
-                    right: item.paddingRight,
-                    bottom: item.paddingBottom,
-                })
-            }
-        }
-    });
-}
+  console.log('paddingControl');
+  const setPadding = (item, paddings) => {
+    console.log('setPadding',item, paddings);
+    item.paddingTop = paddings.top.toString();
+    item.parent.paddingBottom = paddings.bottom;
+    item.parent.paddingLeft = paddings.left;
+    item.parent.paddingRight = paddings.right;
+  };
+  allLayers.forEach((item) => {
+    if (item.name.includes(paddings.type)) {
+      console.log(item);
+      if (!item.layoutMode) {
+        createFrame(item);
+        item.name = item.name.replace(paddings.type, '');
+        item.parent.layoutMode = 'VERTICAL';
+        setPadding(item, paddings);
+      } else {
+        // setPadding(item, paddings);
+        // figma.ui.postMessage({
+        //   top: item.paddingTop,
+        //   left: item.paddingLeft,
+        //   right: item.paddingRight,
+        //   bottom: item.paddingBottom,
+        // });
+      }
+    }
+  });
+};
 
-paddingControl("$1")
+figma.ui.onmessage = (msg) => {
+  if (msg.type === '$1') {
+    console.log(msg.value);
+    $1 = msg.value;
+    paddingControl($1);
+  }
+};
